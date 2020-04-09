00a60475964f08ac203cdae39eaabe303343226d
RameezIsrar <rameezisrarulhaq@gmail.com>
2020-04-09 13:42:08 +0500

    Create a controller that will showcase the user profile and the analytics

---
+++ app/Http/Controllers/ForumProfileController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\ForumActivity;
+use App\Tasks\Forum\TrendingThreads;
+
+class ForumProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user,  TrendingThreads $trendingThreads)
+    {
+        $activies = ForumActivity::feed($user);
+
+
+
+        return view('forum.profiles.show',[
+            'user' => $user,
+            // 'user_threads' => $user->threads()->paginate(10)
+            'activities' => $activies,
+            'trending_threads' => $trendingThreads->get(),
+            'user_forum_info'  => $user->forumInfo
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+}
