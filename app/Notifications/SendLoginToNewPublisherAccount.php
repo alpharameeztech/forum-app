2f8c45c93839fb6e5daa381535ffad3a95a347e6
RameezIsrar <rameezisrarulhaq@gmail.com>
2020-04-09 17:41:18 +0500

    Send logins details with a Notification when a new publisher account gets created

---
+++ app/Notifications/SendLoginToNewPublisherAccount.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class SendLoginToNewPublisherAccount extends Notification
+{
+    use Queueable;
+
+    private $user;
+    /**
+     * Create a new notification instance.
+     *
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->subject('Account Login Details')
+                    ->line('Your account login email:' . $this->user->email . ' and your password:' . $this->user->unencryptedPassword )
+                    ->action('Login', env('APP_URL') . '/login' );
+
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
