8785aa2e256931a8a62a85f35721bd45476d7755
RameezIsrar <rameezisrarulhaq@gmail.com>
2020-04-09 17:50:42 +0500

    Create a service provider to mange repositories

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Providers;
+
+use App\Interfaces\AppearanceRepositoryInterface;
+use App\Interfaces\PublisherRepositoryInterface;
+use App\Interfaces\ReplyRepositoryInterface;
+use App\Interfaces\BrandingRepositoryInterface;
+use App\Interfaces\ThemeSettingRepositoryInterface;
+use App\Repositories\AppearanceRepository;
+use App\Repositories\ChannelRepository;
+use App\Interfaces\ChannelRepositoryInterface;
+use App\Interfaces\ThreadRepositoryInterface;
+use App\Interfaces\MemberRepositoryInterface;
+use App\Repositories\MemberRepository;
+use App\Repositories\PublisherRepository;
+use App\Repositories\ReplyRepository;
+use App\Repositories\BrandingRepository;
+use App\Repositories\ThemeSettingRepository;
+use App\Repositories\ThreadRepository;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            ChannelRepositoryInterface::class,
+            ChannelRepository::class
+        );
+
+        $this->app->bind(
+            ThreadRepositoryInterface::class,
+            ThreadRepository::class
+        );
+
+        $this->app->bind(
+            ReplyRepositoryInterface::class,
+            ReplyRepository::class
+        );
+
+        $this->app->bind(
+            PublisherRepositoryInterface::class,
+            PublisherRepository::class
+        );
+
+        $this->app->bind(
+            MemberRepositoryInterface::class,
+            MemberRepository::class
+        );
+
+        $this->app->bind(
+            ThemeSettingRepositoryInterface::class,
+            ThemeSettingRepository::class
+        );
+
+        $this->app->bind(
+            AppearanceRepositoryInterface::class,
+            AppearanceRepository::class
+        );
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
