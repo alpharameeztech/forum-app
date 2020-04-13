d1fcb6f74f2e74a9a5ab4bd1ae330c3717b179fd
RameezIsrar <rameezisrarulhaq@gmail.com>
2020-04-13 14:59:27 +0500

    Add the right navigation section

---
+++ resources/js/components/navigation/RightNavigation.vue
@@ -0,0 +1,31 @@
+<script>
+
+
+import shopAppearance from "../../mixins/shopAppearance";
+    export default {
+
+        mixins: [shopAppearance],
+
+        data(){
+            return {
+
+
+            };
+        },
+        created(){
+
+
+
+        },
+
+        methods: {
+
+
+
+        },
+
+        mounted() {
+
+        }
+    }
+</script>
