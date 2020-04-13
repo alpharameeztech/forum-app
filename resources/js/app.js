30cadb3a695a3170eed94b50e5272ee2f8f5edbb
RameezIsrar <rameezisrarulhaq@gmail.com>
2020-04-13 14:00:53 +0500

    Import fonts

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import Vuetify from "vuetify";
+import '@mdi/font/css/materialdesignicons.css'
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
