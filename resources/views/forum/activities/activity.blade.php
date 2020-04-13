574b920734eb07384ac7f2324e7b34c7ade94b2a
RameezIsrar <rameezisrarulhaq@gmail.com>
2020-04-13 16:43:55 +0500

    Add a page to show the user activity on the forum

---
+++ resources/views/forum/activities/activity.blade.php
@@ -0,0 +1,46 @@
+<div class="col-sm-12 ">
+    <div class="card thread">
+
+
+      <div class="card-body">
+        
+      
+
+          <div class="row">
+            <div class="col-sm-1"></div>
+            
+            <div class="col-sm-11">
+             
+              
+                <div class="card">
+                    <div class="card-header">
+                        
+                      {{$heading}}
+
+                    </div>
+
+                  <div class="card-body">
+                      <blockquote class="blockquote mb-0">
+                         
+                          
+                        
+                      {{$body}}
+                    </blockquote>
+                  </div>
+                </div>
+
+             
+             
+            </div>
+
+          </div><!-- row ended -->
+
+      </div> <!-- card-body ended -->
+
+
+    </div> <!-- card thread ended -->
+
+
+  </div> <!--col 12 ende --> 
+
+  
