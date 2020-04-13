a3c4717e6763ef36737adfe1c1d530fc86887bbe
RameezIsrar <rameezisrarulhaq@gmail.com>
2020-04-13 12:22:59 +0500

    Get the avatar of the logged in user

---
+++ app/Services/LoggedInUserAvatar.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tasks;
+
+use Illuminate\Support\Facades\Auth;
+use App\UserTrainingHistory;
+use App\Product;
+use App\SubscriptionRepository\UserSubscriptions;
+
+class LoggedInUserAvatar {
+
+   public static function get(){
+
+        return Auth::user()->photo_url;
+
+    }
+
+}
+
+
+
