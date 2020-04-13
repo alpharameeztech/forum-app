f38116f200a1217f979de64b3cacdf71f2035239
RameezIsrar <rameezisrarulhaq@gmail.com>
2020-04-13 15:02:02 +0500

    User can update the avatar

--- resources/js/app.js
+++ resources/js/app.js
@@ -67,6 +67,10 @@ Vue.component(
     'right-navigation',
     require('./components/navigation/RightNavigation').default);
 
+Vue.component(
+    'user-profile',
+    require('./components/UserProfileComponent').default);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
---
+++ resources/js/components/UserProfileComponent.vue
@@ -0,0 +1,37 @@
+<script>
+
+    export default {
+
+        props: [],
+
+        data(){
+
+            return{
+                file: null,
+                imageUrl: null,
+                logo: '',
+                isEditing: false
+            }
+        },
+
+        mounted(){
+
+        },
+        methods: {
+
+            uploadingImage()
+            {
+
+                this.$root.$emit('loading', true);
+
+            },
+
+            save(){
+
+
+            }
+
+        },
+
+    }
+</script>
